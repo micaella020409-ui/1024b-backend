2275b42039e3d13f4bf30b81dfc7d3ec86d168be
micaella020409-ui <micaella.020409@gmail.com>
2026-02-25 11:20:26 -0300

    jkcsbvdjhui

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,61 @@
 let x:number = 10
 
-let nome:string = "Guilherme"
+let nome:string = "MICAELLA"
 
 let vetor:number[] = [1,2,3,4,5]
 
 let nomes:string[] = ["oi","olá"]
 console.log(nomes)
 
+
+
 //Teste
+// let bol:boolean = true 
+
+// bol = false
+
+// const variavel:any = "oi"
+
+// const a:undefined = undefined;
+
+// const obj:{id:number;nome:string;idade:number} = {
+//    id:1,
+//    nome:"MICAELLA",
+//    idade:16
+
+
+// }
+// obj. id=2
+// obj.nome = "MICA"
+// obj.idade=20
+
+
+/////////////////////////////////////
+
+const x1 = 10
+
+const vetor1: number[] = [0,1,2,3,4]
+vetor1[0] = 10
+
+// asdfasdg = [0,1,2,3,4]
+
+let vetor2: number [] = [0,1,2,3,4]
+vetor2[0] = [0,1,2,3,4]
+
+const b: number|string = "MICA" // OU NUMERO
+// asdfasdg = [0,1,2,3,4]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
