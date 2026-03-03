e857f5ffadc5df5659e1c70a2d9e72a5928e6040
micaella020409-ui <micaella.020409@gmail.com>
2026-03-03 11:42:04 -0400

    aula 03/03

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-let x:number = 10
+//let x:number = 10
 
-let nome:string = "MICAELLA"
+//let nome:string = "MICAELLA"
 
-let vetor:number[] = [1,2,3,4,5]
+//let vetor:number[] = [1,2,3,4,5]
 
-let nomes:string[] = ["oi","olá"]
-console.log(nomes)
+//let nomes:string[] = ["oi","olá"]
+//console.log(nomes)
 
 
+
+/////////////////////////////////////
 
 //Teste
 // let bol:boolean = true 
@@ -32,30 +34,46 @@ console.log(nomes)
 
 /////////////////////////////////////
 
-const x1 = 10
+//const x1 = 10
 
-const vetor1: number[] = [0,1,2,3,4]
-vetor1[0] = 10
+//const vetor1: number[] = [0,1,2,3,4]
+//vetor1[0] = 10
 
 // asdfasdg = [0,1,2,3,4]
 
-let vetor2: number [] = [0,1,2,3,4]
-vetor2[0] = [0,1,2,3,4]
+//let vetor2: number [] = [0,1,2,3,4]
+//vetor2[0] = [0,1,2,3,4]
 
-const b: number|string = "MICA" // OU NUMERO
+//const b: number|string = "MICA" // OU NUMERO
 // asdfasdg = [0,1,2,3,4]
 
 
 
+/////////////////////////////////////                                                                         
+
+
+
+//function nomeFuncao(variavel:number):number{
+    //return 10
+
+//}
+
+//const f = nomeFuncao
+//const f3= f(10)
+//console.log("valor de x é"+f3.toString())
+
+
+function somaVetor(vetor:number[]){
+
+let= soma 0
+for (let i = 0; i , vetor.length; i++){
+   const elemente
+
+}
 
 
 
 
-
-
-
-
-
-
+}
 
 
